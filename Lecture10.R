e6f98174852077754133934042405066b98e9018
cookie1905 <neethu51193@gmail.com>
2025-12-01 12:28:44 +0100

    new lecture

--- Lecture10.R
+++ Lecture10.R
@@ -38,8 +38,8 @@ levels(data$rank)
 ?glm
 
 glm.model <- glm(formula = admit ~ gre + gpa + rank,
-    data = data, 
-    family = "binomial")   # response variable is binary (0/1)
+                 data = data, 
+                 family = "binomial")   # response variable is binary (0/1)
 
 summary(glm.model)
 # gra and gpe are significant predictors
@@ -178,3 +178,358 @@ airquality.wide <- airquality.long %>% pivot_wider(
 )
 head(airquality.wide)
 head(airquality)
+
+# Note
+# there is reshape2 older package from the creator of tidyr
+# tidyr is more easy to use
+
+# Common Pitfalls in R Programming
+# ==========================================
+#
+# 1. When converting a factor to numeric go through character
+# factor -> character -> numeric
+# ==========================================
+
+my.fac <- as.factor(c("11", "10", "4", "3", "4"))
+levels(my.fac)                                  # "10" "11" "3"  "4" 
+
+as.numeric(my.fac)                              # 2 1 4 3 4, which is wrong
+
+# use instead
+as.numeric(as.character(my.fac))                # 11 10  4  3  4
+
+# 2. When you know how many elements you have,instead of using list() use
+# ==========================================
+
+vector("list", 5)                               # creates a list of length 5, with each slot initialized to NULL.
+
+# list() creates an empty list of length 0
+# every time you grow the list, R may need to reallocate memory to make space
+# slower
+# using vector("list", length of list) is much faster.
+
+# 3. Always try to work with row names and column names of a matrix instead of positions 
+# ==========================================
+#
+# To remove a single column from a matrix
+my.matrix <- my.matrix[, -which(colnames(my.matrix) == "name of the column you want to remove")]
+
+c1 <- c(100,220, 300)
+c2 <- c(289, 765, 0)
+
+m <- rbind(c1,c2)
+rownames(m) <- c("gene1", "gene2")
+colnames(m) <- c("sample1", "sample2", "sample3")
+m
+
+which(colnames(m) == "sample2")                 # get the index of column with name "sample2"
+m <- m[, -which(colnames(m) == "sample2") ]     # remove that column
+m
+
+# To remove multiple columns from a matrix
+my.matrix <- my.matrix[, -which(colnames(my.matrix) %in% c("column1", "column3", "column5"))]
+
+# To remove multiple rows from a matrix
+my.matrix <- my.matrix[-which(rownames(my.matrix) %in% c("row1", "row3", "row5")), ]
+
+m <- matrix(1:24, nrow = 3)                      # R will automatically set ncol = 8
+rownames(m) <- c("row1", "row2", "row3")
+colnames(m) <- paste("col", 1:8)
+m
+
+m <- m[, -which(colnames(m) %in% c("col 1", "col 3", "col 5"))]
+m
+
+# 4. To force a scientific notation use format()
+format(0.2378345, scientific=T, digits = 3)       # 3 significant digits     
+
+# Can also do the other way around
+format(2.387e-11, scientific=F, digits = 2)       # 2 significant digits (Leading zeros (before the first non‑zero digit) are not significant.)
+
+# 5. strsplit combined with lapply
+# ==========================================
+
+dates <- c(
+  "01-05-1925",
+  "12-14-2021",
+  "03-23-1919",
+  "11-30-1867",
+  "05-20-1994",
+  "07-02-1995",
+  "10-20-1994"
+)
+
+split.dates <- strsplit(dates, "-")
+split.dates                                      # returns a list
+
+# [ is actually a function (called the subsetting operator).
+# so select  3rd element 
+
+years <- unlist(lapply(split.dates, "[", 3))  
+years
+# or 
+# sapply(split.dates, "[", 3)
+
+months <- sapply(split.dates, "[", 1)
+days  <- sapply(split.dates, "[", 2)
+
+# 6 grepl to do partial string match
+# ==========================================
+#
+# get every date with a 20 in it
+
+grepl("20", dates)                      # returns a logical vector
+which(grepl("20", dates))
+dates[which(grepl("20", dates))]
+
+all(which(grepl("20", dates)))          # returns  TRUE if pattern is present in atleast 1 
+
+# grep can return indices directly
+grep("20", dates) == which(grepl("20", dates))
+dates[grep("20", dates)]
+
+
+# 7. Its best to use season as a predictor instead of month
+# ==========================================
+
+# Convert months to season
+to.season <- function(dates, pos=1){ # position of month in your dates
+  months <- as.numeric(sapply(strsplit(dates, "-"), "[", pos))
+  
+  ret <- rep(NA, length(months))
+  ret[months >= 3 & months <= 5] <- "Spring"
+  ret[months >= 6 & months <= 8] <- "Summer"
+  ret[months >= 9 & months <= 11] <- "Fall"
+  ret[months == 12 | months == 1 | months == 2] <- "Winter"
+  
+  return(ret)
+}
+
+to.season(dates)
+
+# To do computation with dates
+date1 <- as.Date("05/11/1993", format = "%d/%m/%Y")
+date1 <- as.Date("05/11/93", format = "%d/%m/%y")
+date2 <- as.Date("23-11-2025", format = "%d-%m-%Y")
+
+class(date1)            # "Date"
+
+# with "Date" objects we can do computations
+date2 - date1           # difference in days
+
+Sys.Date()              # todays date
+
+# can use seq on dates
+
+# give me dates for the next 10 weeks from today
+seq(Sys.Date(), length.out = 10, by = "1 week")
+
+
+# 8. Replace in text vectors
+# gsub() global substitution tool
+# ==========================================
+
+tiger.texts <- c(
+  "The tiger jumps around.",
+  "A tiger is sitting quietly.",
+  "The tiger runs through the grass.",
+  "A tiger lies down to rest.",
+  "The tiger plays with its tail."
+)
+
+cat.texts <- gsub("tiger", "cat", tiger.texts)
+cat.texts
+
+# 9. Order a matrix by column(s)
+
+m <- matrix(
+  c("Tiger", "Lion", "Dog", "Cat", "Lion",
+    "Yawning", "Sleeping", "Barking", "Licking", "Hunting"),
+  nrow = 5,
+  ncol = 2
+)
+colnames(m) <- c("Animal", "Action")
+m
+
+ordering <- order(m[,"Animal"])   # ,desc=T
+m[ordering, ]
+
+
+ordering <- order(m[,"Animal"], m[, "Action"])
+m[ordering, ]
+
+# 10. Preallocate with matrix(), vector(), or data.frame() and fill it in
+# ==========================================
+
+# Go through a matrix and collect multiple statistics per row
+gene.exp <- matrix(
+  c(5.2, 3.1, 4.8,
+    2.4, 6.7, 5.5,
+    7.8, 8.2, 6.9,
+    1.2, 2.3, 1.9),
+  nrow = 4,
+  ncol = 3,
+  byrow = TRUE
+)
+rownames(gene.exp) <- c("GeneA", "GeneB", "GeneC", "GeneD")
+colnames(gene.exp) <- c("Sample1", "Sample2", "Sample3")
+
+exp.stats <- data.frame(Gene = rownames(gene.exp),
+                        Mean = NA,
+                        SD = NA)
+
+for (i in 1:nrow(gene.exp)){
+  exp.stats$Mean[i] <- mean(gene.exp[i,], na.rm = TRUE)
+  exp.stats$SD[i] <- sd(gene.exp[i,], na.rm = TRUE)
+}
+
+exp.stats
+
+rownames(exp.stats) <- exp.stats$Gene
+exp.stats <- exp.stats[, -which(colnames(exp.stats) == "Gene")]
+exp.stats
+
+
+# More simple way to get the stats
+results <- c()
+
+for (i in 1:nrow(gene.exp)){
+  mmean <- mean(gene.exp[i,], na.rm = TRUE)
+  msd <- sd(gene.exp[i,], na.rm = TRUE)
+  results <- rbind(results, c(mmean, msd))
+}
+
+rownames(results) <- rownames(gene.exp)
+colnames(results) <- c("Mean", "SD")
+results
+
+# Always pre allocate 
+big.m <- matrix(runif(1000000), nrow= 10000, ncol = 100)
+
+system.time({
+  results <- c()
+  for (i in 1:nrow(big.m)){
+    mmean <- mean(big.m[i,], na.rm = TRUE)
+    msd <- sd(big.m[i,], na.rm = TRUE)
+    results <- rbind(results, c(mmean, msd))
+  }
+})
+
+# user  system elapsed 
+# 0.42    0.33    0.73 
+
+system.time({
+  results <- matrix(NA, nrow = nrow(big.m), ncol = 2)
+  
+  for (i in 1:nrow(big.m)){
+    mmean <- mean(big.m[i,], na.rm = TRUE)
+    msd <- sd(big.m[i,], na.rm = TRUE)
+    results[i, ] <- c(mmean, msd)
+  }
+})
+
+# user  system elapsed 
+# 0.34    0.00    0.34 
+
+# Pre allocating saves time 
+# So always preallocate with matrix(), vector(), or data.frame() and fill it in
+
+# There is also proc.time
+
+s <- proc.time()
+
+results <- matrix(NA, nrow = nrow(big.m), ncol = 2)
+for (i in 1:nrow(big.m)){
+  mmean <- mean(big.m[i,], na.rm = TRUE)
+  msd <- sd(big.m[i,], na.rm = TRUE)
+  results[i, ] <- c(mmean, msd)
+}
+
+proc.time() - s
+
+# 11. First convert to character before converting to numeric
+# R sometimes convert to factor instead of numeric
+# ==========================================
+
+# Force first three columns of my.matrix into numeric
+my.matrix <- matrix(rep("123", 30), ncol=6, nrow=5)
+my.matrix
+
+num.mat <- apply(my.matrix[,c(1,2,3)], 2, function(x){
+  as.numeric(as.character(x))
+})
+
+class(num.mat[, 1])
+
+# 12. pmatch: partial match
+# ==========================================
+
+example <- function(x, method){
+  # we want to check if the method user input is actually supported by function or not
+  supported <- c("pearson", "spearman", "kendall")
+  method <- pmatch(method, supported)    # returns NA f no match, otherwise returns index of matched
+  if (is.na(method)) stop("Method not supported")
+  return(method)
+  
+  # rest of the logic
+  #
+}
+
+example(c(1:10), "p") # 1, 1st method in supported
+example(c(1:10), "pear")
+example(c(1:10), "ken")
+
+# 13. invisible()
+# ==========================================
+
+example <- function(x){
+  rep(x, 1000000)
+}
+example(1)
+
+# prevents a function’s return value from printing to the console,
+# but still returns it so other code can use it
+
+example <- function(x){
+  invisible(rep(x, 1000000))
+}
+example(1)  # the value is still returned, just not printed.
+
+# Pipelines
+# R is a good lang to write pipelines
+# ==========================================
+#
+# When running R from a server
+# Always end every path with q()
+q("no") # quit without saving the work space
+
+?commandArgs
+# read command line arguments also return script name
+# with  trailingOnly = TRUE, only the arguments after your script name.
+
+cmd.args <- commandArgs(trailingOnly = TRUE)
+if (length(cmd.ar) < 1){
+  stop("Atleast 1 argument is needed")
+  q("no")  # # Always quit a script, otherwise R will continue to run
+}
+
+args <- as.character(cmd.args[1])
+cat(args, "\n")
+
+# Always quit a script, otherwise R will continue to run
+q("no")
+
+# To run on a server
+# Rscript my.script.R
+
+# How to run external commands?
+# example: dir which is a windows command to show content of the current directory
+
+system("dir") # not working? try
+system("cmd /c dir")
+
+files <- system("cmd /c dir", intern = T)
+
+# In R we have similar to dir
+list.files() # print all files in the current working directory.
+
